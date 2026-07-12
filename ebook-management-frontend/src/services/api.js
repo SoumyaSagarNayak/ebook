df8f428e81181c476ef08b709e6a5c525b4befa1
Soumya Sagar Nayak <soumyasagarnayak351@gmail.com>
2026-07-13 00:43:42 +0530

    chore: point frontend API base URL to deployed Render backend

--- ebook-management-frontend/src/services/api.js
+++ ebook-management-frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'https://book-vault-e9tl.onrender.com/api' });
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
